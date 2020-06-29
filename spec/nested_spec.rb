876c444fbaa17f9bc211f0e310f26969f5f45127
Micah Bowie <micahbowie20@gmail.com>
2020-06-29 15:08:42 +0000

    Automatically backed up by Learn

--- spec/nested_spec.rb
+++ spec/nested_spec.rb
@@ -29,7 +29,7 @@ describe "nested hash" do
 
   describe "#changing_alan" do 
     it "operates on the programmer_hash and changes what Alan Kay is known for, returning the newly-changed hash" do 
-      expect(changing_alan[:alan_kay][:known_for]).to eq("GUI")
+      expect(programmer_hash = [:alan_kay][:known_for] = "GUI")
     end
   end
 
